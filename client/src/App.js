c2169bdb018208a148d5e618bf23b77f287521f0
Olloshukur6092 <matnazarovolloshukur7223@gmail.com>
2022-03-12 12:37:25 +0500

    Laravel va Reactdagi loyiha uxshadi.. 2022-03-12

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import EditProduct from "./components/product/edit.component";
+import ProductList from "./components/product/list.component";
+import CreateProduct from "./components/product/create.component";
 
 function App() {
   return (
-    <div className="App">
-      Laravel And React...
-    </div>
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            Basic Crud App
+          </Link>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route path="/product/create" element={<CreateProduct />} />
+              <Route path="/product/edit/:id" element={<EditProduct />} />
+              <Route exact path="/" element={<ProductList />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
